fc85cbe21e1d8ccbedd25586f9d5114016353b62
lei <mia.wang90@gmail.com>
2022-11-14 18:31:07 +0900

    Modify initFirebase

---
+++ src/utils/firebase/initFirebase.ts
@@ -0,0 +1,25 @@
+import { initializeApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSEGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+// 複数回呼ばれるとエラー
+export function initFirebase() {
+  if (!getApps().length) {
+    initializeApp(firebaseConfig);
+  }
+}
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth();
+
+export default firebaseConfig;
