75903c2bcfd4f9fb7bf516e9bc6f14cc16e06f24
lei <mia.wang90@gmail.com>
2022-11-30 00:24:05 +0900

    Setup NextUI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { NextUIProvider } from "@nextui-org/react";
+import axios from "axios";
+
+import "../styles/globals.css";
+import { Layout } from "components/layouts/Layout";
+
+axios.defaults.baseURL =
+  process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000/api/v1";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <NextUIProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </NextUIProvider>
+  );
 }
