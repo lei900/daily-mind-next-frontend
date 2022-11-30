199050ee8c43de673dfcfb31837608b2dfa0b2ce
lei <mia.wang90@gmail.com>
2022-11-30 22:56:21 +0900

    Fix src type error

--- src/components/layouts/UserMenu.tsx
+++ src/components/layouts/UserMenu.tsx
@@ -24,7 +24,7 @@ const UserMenu = ({ currentUser, onLogout }: Props) => {
               as="button"
               color="secondary"
               size="md"
-              src={currentUser.photoURL}
+              src={currentUser.photoURL!}
             />
           </Dropdown.Trigger>
         </Navbar.Item>
