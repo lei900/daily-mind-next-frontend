1069ea8781c6c54d6659ef8ff8871223d65c68c7
lei <mia.wang90@gmail.com>
2023-02-10 20:06:40 +0900

    feat: set cookies maxage to 30 days

--- src/utils/manageCookies.ts
+++ src/utils/manageCookies.ts
@@ -3,14 +3,20 @@ import nookies from "nookies";
 import { UserInfo } from "types/types";
 
 export const setUserInfoCookies = (userInfo: UserInfo) => {
-  setCookie(null, "uid", userInfo.uid, { path: "/" });
+  setCookie(null, "uid", userInfo.uid, {
+    maxAge: 30 * 24 * 60 * 60,
+    path: "/",
+  });
   setCookie(null, "avatar", userInfo.avatar || "", {
+    maxAge: 30 * 24 * 60 * 60,
     path: "/",
   });
   setCookie(null, "nickname", userInfo.nickname, {
+    maxAge: 30 * 24 * 60 * 60,
     path: "/",
   });
   setCookie(null, "bio", userInfo.bio || "", {
+    maxAge: 30 * 24 * 60 * 60,
     path: "/",
   });
 };
